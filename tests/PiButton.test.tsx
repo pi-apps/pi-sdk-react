799b9ef3fc3b6530e528bd765571139128bba604
John Kolen <john@minepi.com>
2025-12-16 18:07:11 -0600

    NextJS support added

--- tests/PiButton.test.tsx
+++ tests/PiButton.test.tsx
@@ -2,31 +2,44 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import PiButton from '../PiButton';
 import '@testing-library/jest-dom';
+import PiSdkBase from 'pi-sdk-js';
 
-describe('PiButton', () => {
-  it('renders a disabled button if not connected', () => {
-    jest.spyOn((PiButton.prototype as any), 'is_connected_to_pi').mockReturnValue(false);
-    const TypedPiButton = PiButton as unknown as React.ComponentType<any>;
-    const { getByText } = render(<TypedPiButton />);
-    const button = getByText(/buy/i);
+describe('PiButton (functional)', () => {
+  let connectMock: jest.SpyInstance;
+  let createPaymentMock: jest.SpyInstance;
+
+  beforeEach(() => {
+    // Mock connect to always call onConnection and resolve
+    connectMock = jest.spyOn(PiSdkBase.prototype, 'connect').mockImplementation(function (this: any) {
+      if (this.onConnection) this.onConnection();
+      return Promise.resolve();
+    });
+    createPaymentMock = jest.spyOn(PiSdkBase.prototype, 'createPayment').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('renders a disabled button if not connected', async () => {
+    // Simulate never connecting: do not call onConnection
+    connectMock.mockImplementationOnce(() => Promise.resolve());
+    const { findByText } = render(<PiButton />);
+    const button = await findByText(/buy/i);
     expect(button).toBeDisabled();
   });
 
-  it('renders an enabled button if connected', () => {
-    jest.spyOn((PiButton.prototype as any), 'is_connected_to_pi').mockReturnValue(true);
-    const TypedPiButton = PiButton as unknown as React.ComponentType<any>;
-    const { getByText } = render(<TypedPiButton />);
-    const button = getByText(/buy/i);
+  it('renders an enabled button if connected', async () => {
+    const { findByText } = render(<PiButton />);
+    const button = await findByText(/buy/i);
     expect(button).not.toBeDisabled();
   });
 
-  it('runs buy() on click when connected', () => {
-    jest.spyOn((PiButton.prototype as any), 'is_connected_to_pi').mockReturnValue(true);
-    const buySpy = jest.spyOn((PiButton.prototype as any), 'buy').mockImplementation(() => {});
-    const TypedPiButton = PiButton as unknown as React.ComponentType<any>;
-    const { getByText } = render(<TypedPiButton />);
-    const button = getByText(/buy/i);
+  it('runs createPayment on click when connected', async () => {
+    const { findByText } = render(<PiButton />);
+    const button = await findByText(/buy/i);
+    expect(button).not.toBeDisabled();
     fireEvent.click(button);
-    expect(buySpy).toHaveBeenCalled();
+    expect(createPaymentMock).toHaveBeenCalled();
   });
 });
